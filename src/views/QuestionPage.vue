f2b01647e99700bf83ab045ad23efab3cc4f5eb9
Manuel Zamudio <mzamudio.trevino@gmail.com>
2025-10-19 14:47:10 -0700

    question and answer styling is done.

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -12,48 +12,53 @@ const answers = ref([]);
 onMounted(async () => {
   question.value = await api.getQuestion(route.params.id);
 
-  
+  // Push correct answer
   answers.value.push({
     id: answers.value.length,
     correct: true,
     answer: question.value.correct_answer
   });
 
-  
+  // Push incorrect answers
   question.value.incorrect_answers.forEach((wrong_answer) => {
     answers.value.push({
       id: answers.value.length,
       correct: false,
       answer: wrong_answer
     });
-  
   });
 
-  answers.value =shuffle(answers.value)
- // console.log(question.value)
-
+  // Shuffle answers
+  answers.value = shuffle(answers.value);
 });
 
 const shuffle = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    //swap
+    // swap
     [array[i], array[j]] = [array[j], array[i]];
   }
 
-  return array
-}
-
-
+  return array;
+};
 </script>
 
 <template>
-  <div v-if="question" class="">
+  <div v-if="question" class="flex h-full w-full flex-col items-center gap-8 p-10">
     <BaseTitle>{{ question.category }}</BaseTitle>
-    {{ question.question }}
 
-    <div v-for="answer in answers" v-html="answer.answer" :key="answer.id" class=""></div>
+    <div v-html="question.question" class="text-center text-2xl font-bold"></div>
+
+    <div class="grid w-full flex-grow grid-cols-2 gap-8">
+      <div
+        v-for="answer in answers"
+        v-html="answer.answer"
+        :key="answer.id"
+        class="bg-green-500 flex items-center justify-center text-4xl rounded-lg text-white py-10 px-2"
+      ></div>
+    </div>
   </div>
+
   <div v-else class="">
     Loading...
   </div>
