86759e5777ed22b21dad298655af5c0a364a9d48
Manuel Zamudio <mzamudio.trevino@gmail.com>
2025-10-19 13:59:20 -0700

    Fixed Question

--- src/components/BaseTitle.vue
+++ src/components/BaseTitle.vue
@@ -1,9 +1,7 @@
 <template>
-    <div class="flex items-center justify-center gap-4">
-        <h1 class="text-6x1 font-thin text-slate 700 uppercase">
-
-            <slot/>
-
-        </h1>
-    </div>
-    </template>
+  <div class="flex items-center justify-center gap-4">
+    <h1 class="text-6xl font-thin text-slate-700 uppercase text-center tracking-wide">
+      <slot /> 
+    </h1>
+  </div>
+</template>
