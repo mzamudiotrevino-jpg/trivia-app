0244bf1d333e7debf6a48f7034f7d75e418e0633
Manuel Zamudio <mzamudio.trevino@gmail.com>
2025-10-19 13:39:46 -0700

    GetQuestion is working

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/trivia-app/'
-
+  base: '/trivia-app/',
   plugins: [vue()],
   resolve: {
     alias: {
