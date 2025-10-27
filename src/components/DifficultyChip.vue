4999fdb49bcae147ad113e1f3041c3f13c510286
Manuel Zamudio <mzamudio.trevino@gmail.com>
2025-10-26 18:21:48 -0700

    difficulty added

---
+++ src/components/DifficultyChip.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+  difficulty: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <p
+    class="px-5 py-2 rounded-full fixed uppercase text-white bottom-6 left-8 text-xl font-bold shadow-lg z-50"
+    :class="props.difficulty === 'easy' ? 'bg-green-600' : props.difficulty === 'medium' ? 'bg-yellow-500' : 'bg-red-600'"
+  >
+    {{ props.difficulty }}
+  </p>
+</template>
--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -3,7 +3,7 @@ import { onMounted, ref } from 'vue';
 import useAPI from '@/composables/useAPI';
 import { useRoute } from 'vue-router';
 import BaseTitle from '@/components/BaseTitle.vue';
-
+import DifficultyChip from '@/components/DifficultyChip.vue';
 const api = useAPI();
 const question = ref(null);
 const route = useRoute();
@@ -12,14 +12,14 @@ const answers = ref([]);
 onMounted(async () => {
   question.value = await api.getQuestion(route.params.id);
 
-  // Push correct answer
+  
   answers.value.push({
     id: answers.value.length,
     correct: true,
     answer: question.value.correct_answer
   });
 
-  // Push incorrect answers
+  
   question.value.incorrect_answers.forEach((wrong_answer) => {
     answers.value.push({
       id: answers.value.length,
@@ -28,14 +28,14 @@ onMounted(async () => {
     });
   });
 
-  // Shuffle answers
+  
   answers.value = shuffle(answers.value);
 });
 
 const shuffle = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    // swap
+    
     [array[i], array[j]] = [array[j], array[i]];
   }
 
@@ -44,7 +44,7 @@ const shuffle = (array) => {
 </script>
 
 <template>
-  <div v-if="question" class="flex h-full w-full flex-col items-center gap-8 p-10">
+  <div v-if="question" class="relative flex h-full w-full flex-col items-center gap-8 p-10">
     <BaseTitle>{{ question.category }}</BaseTitle>
 
     <div v-html="question.question" class="text-center text-2xl font-bold"></div>
@@ -57,6 +57,7 @@ const shuffle = (array) => {
         class="bg-green-500 flex items-center justify-center text-4xl rounded-lg text-white py-10 px-2"
       ></div>
     </div>
+    <DifficultyChip :difficulty="question.difficulty"></DifficultyChip>
   </div>
 
   <div v-else class="">
